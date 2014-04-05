a92c5b1e33882708a60318e8b0a82ba72982e3ba
Kevin Conway <kevinjacobconway@gmail.com>
2014-04-05 17:55:51 -0500

    Add: Bind method
    This method is a useful helper for creating bound versions of a
    function to defer.
    
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

--- test/defer.spec.js
+++ test/defer.spec.js
@@ -21,6 +21,7 @@
         it('exposes a specification compliant interface', function () {
 
           expect(typeof defer).to.be("function");
+          expect(typeof defer.bind).to.be("function");
 
         });
 
@@ -39,6 +40,27 @@
           });
 
           expect(test_object.test).to.be(undefined);
+
+        });
+
+        it('provides a bind implementation', function () {
+
+          var specialContext = {},
+            boundCheckArgs;
+
+          function checkArgs() {
+
+            var args = Array.prototype.slice.call(arguments);
+            expect(this).to.be(specialContext);
+            expect(args.length).to.be(3);
+            expect(args[0]).to.be(true);
+            expect(args[1]).to.be(false);
+            expect(args[2]).to.be('test');
+
+          }
+
+          boundCheckArgs = defer.bind(checkArgs, specialContext, true, false);
+          boundCheckArgs('test');
 
         });
 
